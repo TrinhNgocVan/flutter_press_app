458c261f274e92c206d21d2d897849c6fab6e997
vantn93 <sainteric93hd@gmail.com>
2024-09-21 14:51:40 +0700

    feat: add stackDemo page

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -18,8 +18,10 @@ class Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // tránh cho on-screen keyboad đè lên content
       resizeToAvoidBottomInset: false,
       body: SizedBox(
+        // hộp trong suốt  != container (thiết lập được màu  , margin , padding)
         width: double.infinity,
         height: MediaQuery.of(context).size.height,
         child: Stack(
@@ -28,10 +30,7 @@ class Background extends StatelessWidget {
             Positioned(
               top: 0,
               left: 0,
-              child: Image.asset(
-                topImage,
-                width: 120,
-              ),
+              child: Image.asset(topImage, width: 120),
             ),
             Positioned(
               bottom: 0,
@@ -45,3 +44,30 @@ class Background extends StatelessWidget {
     );
   }
 }
+
+
+// class StackDemo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         alignment: Alignment.centerRight,
+//         children: <Widget>[
+//           Container(
+//             color: Colors.green,
+//           ),
+//           Container(
+//             color: Colors.blue,
+//             height: 300.0,
+//             width: 300.0,
+//           ),
+//           Container(
+//             color: Colors.pink,
+//             height: 150.0,
+//             width: 150.0,
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
