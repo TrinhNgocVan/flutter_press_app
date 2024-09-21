4d6906b4d2eb1a3781509941470eac9fac3c42a0
vantn93 <sainteric93hd@gmail.com>
2024-09-21 14:23:22 +0700

    feat: add background content

---
+++ lib/common/constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+const double defaultPadding= 16.0;
+class ImageUrl {
+  static const String topImageUrl  = 'assets/images/main_top.png';
+  static const String bottomImageUrl  = 'assets/images/login_bottom.png'; // todo  : recheck
+}
